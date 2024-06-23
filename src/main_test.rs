db5dd2ebddba577da196b41d9429c8b5493448c2
Chetan Sharma <chetan2309@gmail.com>
2024-06-23 23:33:29 +0530

    Improved test case to use utility functions

--- src/main_test.rs
+++ src/main_test.rs
@@ -1,29 +1,20 @@
 #[cfg(test)]
 mod tests {
     use std::{
-        fs::{self, File, OpenOptions},
+        fs::{self},
         io::{Read, Seek, SeekFrom},
     };
 
-    use crate::SStStorage;
+    use crate::{open_file_read_only, open_file_read_write, SStStorage};
     #[test]
     fn test_write() {
         // Create a temporary file for testing
         let temp_file_path = "temp_test_file.txt";
-        let file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(temp_file_path)
-            .expect("Failed to create temp file");
+        let file = open_file_read_write(temp_file_path).expect("Failed to create temp file");
         let mut sst_storage = SStStorage::new(file);
 
         let key = vec![1, 2, 3];
         let value = vec![4, 5, 6];
-
-        // Clone the key and value before passing to the write method
-        // let key_clone = key_value.key.clone();
-        // let value_clone = key_value.value.clone();
 
         // Call the write method and validate the result
         let result = sst_storage.write(&key, &value, false, None);
@@ -31,7 +22,7 @@ mod tests {
 
         // Manually read the content from the temporary file and validate
         let mut file_content = Vec::new();
-        let mut file = File::open(temp_file_path).expect("Failed to open temp file");
+        let mut file = open_file_read_only(temp_file_path).expect("Failed to open temp file");
         file.seek(SeekFrom::Start(0)).expect("Failed to seek file");
         file.read_to_end(&mut file_content)
             .expect("Failed to read file");
